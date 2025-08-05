ac87d2655ae6c34173482c65345eeccf8c56c487
Gaëtan de Villèle <gdevillele@proton.me>
2025-08-05 15:40:32 +0200

    make cookie_clicker a solo world

--- aduermael/cookie clicker.lua
+++ aduermael/cookie clicker.lua
@@ -1,9 +1,25 @@
+--
+-- Cookie Clicker
+--
+-- original author: aduermael
+--
+
 Config = {
 	Map = nil,
 	Items = { "aduermael.cookie_no_chips", "aduermael.cookie_chip" },
 }
 
+-- -------------------------
+-- global variables
+-- -------------------------
+cooldown_duration = 3 -- seconds
+count = 0 -- counter of clicks (actual value)
+coolingDown = false -- true if we are cooling down, so we don't update the score too often
+dirty = false -- true if we need to update the score once the cooldown is over
+
 Client.OnStart = function()
+	Client.resetScene()
+
 	-- AMBIANCE
 	Sky.SkyColor = Color(59, 29, 103)
 	Sky.HorizonColor = Color(143, 65, 181)
@@ -65,8 +81,6 @@ Client.OnStart = function()
 
 	totalDT = 0.0
 
-	count = 0
-
 	-- UI
 	local ui = require("uikit")
 	countLabel = ui:createText("" .. math.floor(count), Color.White, "big")
@@ -90,15 +104,12 @@ Client.OnStart = function()
 		layoutLabels()
 	end
 	layoutLabels()
-end
 
-Client.ServerConnectionSuccess = function()
-	-- print("ServerConnectionSuccess")
-	-- PING THE SERVER
-	Timer(0.5, function()
-		local e = Event()
-		e.action = "didStart"
-		e:SendTo(Server)
+	getPersonalBest(function(ok, count)
+		if ok then
+			bestCount = count
+			bestLabel.Text = "Best: " .. math.floor(count)
+		end
 	end)
 end
 
@@ -155,15 +166,13 @@ Pointer.Down = function(e)
 
 		if bestCount ~= nil and count > bestCount then
 			bestCount = count
-			bestLabel.Text = "Best: " .. math.floor(count) .. " (" .. Player.Username .. ")"
+			bestLabel.Text = "Best: " .. math.floor(count) -- .. " (" .. Player.Username .. ")"
 		end
 
 		layoutLabels()
 
-		local e = Event()
-		e.action = "add"
-		e.count = add
-		e:SendTo(Server)
+		print("-> updatePersonalBest: from pointer down")
+		updatePersonalBest()
 	end
 end
 
@@ -172,164 +181,119 @@ Client.Tick = function(dt)
 	container.Rotation = Number3(math.sin(totalDT), math.sin(totalDT * 1.3), 0) * 0.2
 end
 
-Client.DidReceiveEvent = function(e)
-	if e.info == "best" then
-		bestCount = e.count
-		bestLabel.Text = "Best: " .. math.floor(e.count) .. " (" .. e.name .. ")"
-		layoutLabels()
-	else
-		if e.count ~= nil then
-			count = count + e.count
-			countLabel.Text = "" .. math.floor(count)
-			layoutLabels()
-		end
-	end
-end
-
+-- hide the directional pad
 Client.DirectionalPad = nil
 
 --
--- Server code
+-- Utilities
 --
 
-Server.Tick = function(dt)
-	-- Block executed once on first tick.
-	-- It's a trick, waiting for Server.OnStart
-	-- to be available.
-	if didStart == nil then
-		didStart = true
+Client.resetScene = function()
+	-- reset scene
+	World:Recurse(function(o)
+		if o.Name == "aduermael.baseplate" then -- or typeof(o) == "Light" or typeof(o) == "Camera" and o ~= Camera
+			o:RemoveFromParent()
+		end
+	end)
+	-- Clouds.On = false
+	-- Player:RemoveFromParent()
+	-- Map:RemoveFromParent()
+	-- Camera.Behavior = nil
+	-- Config.ConstantAcceleration = { 0, 0, 0 }
+end
 
-		playerCounts = {}
+--
+-- Key-Value Store utilities
+--
 
-		function save()
-			-- print("SAVE")
+-- update personal best score in KV store
+-- This can be called anywhere and anytime. It takes care of the cooldown and the dirty flags.
+function updatePersonalBest()
+	local userID = Player.UserID
+	local newCount = count
 
-			for userID, playerData in pairs(playerCounts) do
-				-- print("    userID:", userID)
-				-- print("    playerData.name:", playerData.name)
-				-- print("    playerData.diff:", playerData.diff)
-				-- print("    playerData.count:", playerData.count)
+	if coolingDown then -- already cooling down, so we need to wait for the cooldown to finish
+		print("-> updatePersonalBest: already cooling down, setting dirty flag")
+		dirty = true
+		return
+	else -- not cooling down, so we start the cooldown and update the score
+		coolingDown = true
+		print("-> updatePersonalBest: not cooling down, starting cooldown")
+	end
 
-				if playerData.diff > 0 and playerData.count ~= nil then
-					playerData.count = playerData.count + playerData.diff
-					playerData.diff = 0
-
-					local store = KeyValueStore(userID)
-					store:Set("count", playerData.count, function(success)
-						-- print("SET SUCCESS:", success)
-					end)
-
-					if best.count ~= nil and playerData.count > best.count then
-						updateBest(playerData.count, playerData.name)
-					end
-				end
-			end
+	-- update KV store
+	local store = KeyValueStore(userID)
+	store:Get("count", function(ok, res)
+		if not ok then
+			error("failed to get count from KV store")
 		end
 
-		kTriggerDelta = 5.0 -- saves every 5 seconds
-		trigger = kTriggerDelta
+		print("-> updatePersonalBest: got count from KV store", res.count, newCount)
 
-		best = {
-			count = nil,
-			name = nil,
-		}
-
-		local globalStore = KeyValueStore("global")
-		globalStore:Get("count", "name", function(success, res)
-			if success then
-				if res.count ~= nil then
-					best.count = res.count
+		if res.count == nil or res.count < newCount then
+			store:Set("count", newCount, function(ok)
+				if ok then
+					print("updatePersonalBest: set count to", newCount)
 				else
-					best.count = 0
+					print("updatePersonalBest: failed to set count to", newCount)
 				end
-				if res.name ~= nil then
-					best.name = res.name
-				else
-					best.name = "TEST"
-				end
-			end
-		end)
 
-		function sendBest(recipient)
-			-- print("sendBest:", best.count, best.name)
-			if best.count ~= nil and best.name ~= nil then
-				local e = Event()
-				e.info = "best"
-				e.count = best.count
-				e.name = best.name
-				e:SendTo(recipient)
-			end
-		end
-
-		function updateBest(count, name)
-			local globalStore = KeyValueStore("global")
-
-			globalStore:Get("count", "name", function(success, res)
-				if success then
-					-- only update if the new count is higher
-					if res.count == nil or res.count < count then
-						globalStore:Set("count", count, "name", name, function(success)
-							if success then
-								best.count = count
-								best.name = name
-							end
-						end)
-					else
-						best.count = res.count
-						best.name = res.name
+				-- reset dirty flag right now and cooldown flag in a few seconds
+				dirty = false
+				Timer(cooldown_duration, function()
+					coolingDown = false
+					if dirty then
+						print("-> updatePersonalBest: from timer + dirty")
+						updatePersonalBest()
 					end
+				end)
+			end)
+		else
+			-- reset dirty flag right now and cooldown flag in a few seconds
+			dirty = false
+			Timer(cooldown_duration, function()
+				coolingDown = false
+				if dirty then
+					print("-> updatePersonalBest: from timer + dirty")
+					updatePersonalBest()
 				end
 			end)
 		end
-	end
-
-	trigger = trigger - dt
-	if trigger <= 0 then
-		trigger = kTriggerDelta
-		save()
-	end
+	end)
 end
 
-Server.DidReceiveEvent = function(e)
-	-- print("receiving event from", e.Sender.UserID)
-	-- print("action:", e.action)
-
-	local playerData = playerCounts[e.Sender.UserID]
-
-	-- no matter the event, we need a table to store the count
-	if playerData == nil then
-		-- print("create player counts table")
-		-- if this is the first event from this player,
-		-- create the entry, store the diff, and get
-		-- he actual count from the key/value store.
-		playerCounts[e.Sender.UserID] = { diff = 0, count = 0, name = e.Sender.Username }
-		playerData = playerCounts[e.Sender.UserID]
-	end
-
-	if e.action == "didStart" then
-		-- set best score
-		sendBest(e.Sender)
-
-		local store = KeyValueStore(e.Sender.UserID)
-
-		local callback = function(success, results)
-			if success == true then
-				if results.count == nil then
-					-- print("COUNT IS NIL")
-					playerData.count = 0
-				else
-					-- print("COUNT:", results.count)
-					playerData.count = results.count
-				end
-			end
-
-			local response = Event()
-			response.count = playerData.count
-			response:SendTo(e.Sender)
-		end
-
-		store:Get("count", callback)
-	elseif e.action == "add" then
-		playerData.diff = playerData.diff + e.count
-	end
+-- get personal best score from KV store
+-- callback(ok, count)
+function getPersonalBest(callback)
+	local userID = Player.UserID
+	local store = KeyValueStore(userID)
+	store:Get("count", function(ok, res)
+		callback(ok, res.count or 0)
+	end)
 end
+
+-- -- also try to update the global best
+-- function updateGlobalBest()
+-- 	local userName = Player.Username
+-- 	local newCount = count
+
+-- 	local globalStore = KeyValueStore("global")
+-- 	globalStore:Get("count", "name", function(success, res)
+-- 		if success then
+-- 			if res.count == nil or res.count < newCount then
+-- 				globalStore:Set("count", newCount, "name", userName, function(ok)
+-- 					if ok then
+-- 						-- TODO: update UI
+-- 					end
+-- 				end)
+-- 			end
+-- 		end
+-- 	end)
+-- end
+
+-- function getGlobalBest(callback)
+-- 	local globalStore = KeyValueStore("global")
+-- 	globalStore:Get("count", "name", function(success, res)
+-- 		callback(success, res.count or 0, res.name or "Unknown")
+-- 	end)
+-- end
